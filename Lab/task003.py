bce92137dbff8a5e3bdb21de28d55fede095ec7a
Madhumita <madhumitakul@gmail.com>
2025-12-02 17:52:43 +0530

    Sample Files

---
+++ Lab/task003.py
@@ -0,0 +1,7 @@
+#write python prgm to calculate the Area of circle
+#given its radius and formula area = pie*r^2 (pie =3.14)
+
+r = float(input("Enter radius: "))
+pie = 3.14
+output = pie*(r**2)
+print(output)
